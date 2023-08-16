e6a0c7ee59e6c839c3093fd65addb395c4ceb58c
Nipun Theekshana <theekshana2@gmail.com>
2023-08-16 22:54:09 +0200

    change bootnode URL

--- node1/node.sh
+++ node1/node.sh
@@ -10,7 +10,7 @@ geth --datadir /debug/node1 \
  --http.port 8040 \
  --authrpc.port 8551 \
  --http.api "personal,eth,net,web3,txpool,miner,admin" \
- --bootnodes "enode://9e5de7d01392bc21842118cad2a059faf8a86352d41bd412bbbc04f3e8694604a75bcb6f3566822101887dec6cec904c36121d032faba6bdf73e9e2b9e4eee28@bootnode:30310" \
+ --bootnodes "enode://9e5de7d01392bc21842118cad2a059faf8a86352d41bd412bbbc04f3e8694604a75bcb6f3566822101887dec6cec904c36121d032faba6bdf73e9e2b9e4eee28@127.0.0.1:30310" \
  --http.corsdomain "\*" \
  --networkid 10000 \
  --unlock "0x11aCb7500E281Fc6C881C6067005233E8dc10708" \
