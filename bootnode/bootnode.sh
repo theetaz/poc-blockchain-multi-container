fe62c48282b62dd803155f9eac339fe92c00529a
Nipun Theekshana <theekshana2@gmail.com>
2023-08-16 21:24:07 +0200

    change logfile to print the value

--- bootnode/bootnode.sh
+++ bootnode/bootnode.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 logfile="/debug/debug.log" # Name of the log file
-bootnode -nodekey /app/boot.key -verbosity 9 -addr :30310 >> $logfile
+
+# Start the bootnode command and append its output to the log file
+bootnode -nodekey /app/boot.key -verbosity 9 -addr :30310 >> $logfile 2>&1 &
+
+# Print the content of the log file to the console
+tail -f $logfile
