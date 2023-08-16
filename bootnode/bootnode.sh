51f4800e609af67cb41a7a7bbb9a23fe79a6c6df
Nipun Theekshana <theekshana2@gmail.com>
2023-08-16 21:04:10 +0200

    change directory

--- bootnode/bootnode.sh
+++ bootnode/bootnode.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-logfile="app/debug.log" # Name of the log file
-bootnode -nodekey app/boot.key -verbosity 9 -addr :30310 >> $logfile
+logfile="/app/debug.log" # Name of the log file
+bootnode -nodekey /app/boot.key -verbosity 9 -addr :30310 >> $logfile
