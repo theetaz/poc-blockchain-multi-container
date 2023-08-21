36e090059401fca72094603fa9164ab6e8d5d74d
Nipun Theekshana <theekshana2@gmail.com>
2023-08-21 10:31:30 +0200

    add testing container

---
+++ udp-testing/client.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const dgram = require("dgram");
+const app = express();
+const port = 4500;
+
+let udpSuccess = false;
+
+const client = dgram.createSocket("udp4");
+client.on("message", () => {
+  udpSuccess = true;
+});
+
+app.get("/test-udp", (req, res) => {
+  const message = Buffer.from("Test message");
+  client.send(message, 30310, "bootnode", (error, data) => {
+    console.log("ERROR", error);
+    console.log("DATA", data);
+    if (data) {
+      res.status(200).json({
+        status: "success",
+        message: `UDP communication is successful data : ${data}`
+      });
+    }
+
+    if (error) {
+      res.status(400).json({
+        status: "error",
+        message: `UDP communication is failed error : ${error}`
+      });
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
